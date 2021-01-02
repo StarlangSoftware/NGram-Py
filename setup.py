afb5bdc485a0966740cfb9a7b41c80ba4db56dfb
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:07:31 +0300

    Updated e-mail and github repo url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-NGram',
     version='1.0.14',
     packages=['NGram', 'test'],
-    url='https://github.com/olcaytaner/NGram-Py',
+    url='https://github.com/StarlangSoftware/NGram-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NGram library',
     install_requires=['NlpToolkit-DataStructure', 'NlpToolkit-Sampling']
 )
