8afb14675e01e5760b1f65a67ea42d173aadb5eb
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:42:04 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-NGram',
-    version='1.0.15',
+    version='1.0.16',
     packages=['NGram', 'test'],
     url='https://github.com/StarlangSoftware/NGram-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NGram library',
-    install_requires=['NlpToolkit-DataStructure', 'NlpToolkit-Sampling']
+    install_requires=['NlpToolkit-DataStructure', 'NlpToolkit-Sampling'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
